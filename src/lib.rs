8db971e5661bef27064e641b1b078030f3f292f9
Kyryh <71778401+Kyryh@users.noreply.github.com>
2025-12-18 17:45:06 +0100

    Hook XInputGetState function

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,13 @@
 use std::ffi::c_void;
 
+use retour::StaticDetour;
 use windows::Win32::{
     Foundation::HINSTANCE,
-    System::{Console::AllocConsole, SystemServices::DLL_PROCESS_ATTACH},
-    UI::Input::{KeyboardAndMouse::GetKeyState, XboxController::XINPUT_GAMEPAD},
+    System::{
+        Console::AllocConsole,
+        SystemServices::{DLL_PROCESS_ATTACH, DLL_PROCESS_DETACH},
+    },
+    UI::Input::{KeyboardAndMouse::GetKeyState, XboxController::XINPUT_STATE},
 };
 
 /// Returns 1 if pressed, 0 if not pressed
@@ -11,22 +15,42 @@ fn key_pressed(key: char) -> u16 {
     unsafe { GetKeyState(key as i32) as u16 >> 15 }
 }
 
-pub fn inject_keyboard_input(dw_user_index: u32, p_state: &mut XINPUT_GAMEPAD) {
-    if dw_user_index == 0 {
-        p_state.wButtons.0 |= 0b0
+pub static HOOK: StaticDetour<unsafe extern "system" fn(u32, *mut XINPUT_STATE) -> u32> = {
+    #[inline(never)]
+    unsafe extern "system" fn ffi_detour(dwuserindex: u32, pstate: *mut XINPUT_STATE) -> u32 {
+        (HOOK.__detour())(dwuserindex, pstate)
+    }
+    StaticDetour::__new(ffi_detour)
+};
+
+windows::core::link!("xinput1_4.dll" "system" fn XInputGetState(dwuserindex: u32, pstate: *mut XINPUT_STATE) -> u32);
+
+pub fn xinput_get_state_hook(dw_user_index: u32, pstate: *mut XINPUT_STATE) -> u32 {
+    let result = unsafe { HOOK.call(dw_user_index, pstate) };
+    if dw_user_index == 0
+        && let Some(pstate) = unsafe { pstate.as_mut() }
+    {
+        pstate.Gamepad.wButtons.0 |= 0b0
             | key_pressed('W') << 0
             | key_pressed('S') << 1
             | key_pressed('A') << 2
             | key_pressed('D') << 3;
     }
+    result
 }
 
 #[unsafe(no_mangle)]
 pub extern "system" fn DllMain(_: HINSTANCE, fdw_reason: u32, _: *const c_void) -> bool {
-    if fdw_reason == DLL_PROCESS_ATTACH {
-        unsafe {
+    match fdw_reason {
+        DLL_PROCESS_ATTACH => unsafe {
             AllocConsole().unwrap();
-        }
+            HOOK.initialize(XInputGetState, xinput_get_state_hook)
+                .unwrap()
+                .enable()
+                .unwrap();
+        },
+        DLL_PROCESS_DETACH => unsafe { HOOK.disable().unwrap() },
+        _ => {}
     }
     true
 }
