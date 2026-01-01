020bc3c9b97dced02d6c4d8368abaa84428b6d4f
Kyryh <71778401+Kyryh@users.noreply.github.com>
2026-01-01 19:05:03 +0100

    Change how the XInputGetState function is found
    Taken directly from https://github.com/DavidAngell/rusty-xinput-detour/blob/main/xinput_detour_dll/src/lib.rs#L75

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,21 @@
-use std::ffi::c_void;
+use std::{
+    ffi::{CStr, OsStr, c_void},
+    mem,
+    os::windows::ffi::OsStrExt,
+};
 
 use retour::StaticDetour;
-use windows::Win32::{
-    Foundation::HINSTANCE,
-    System::{
-        Console::AllocConsole,
-        SystemServices::{DLL_PROCESS_ATTACH, DLL_PROCESS_DETACH},
+use windows::{
+    Win32::{
+        Foundation::HINSTANCE,
+        System::{
+            Console::AllocConsole,
+            LibraryLoader::{GetModuleHandleW, GetProcAddress},
+            SystemServices::{DLL_PROCESS_ATTACH, DLL_PROCESS_DETACH},
+        },
+        UI::Input::{KeyboardAndMouse::GetKeyState, XboxController::XINPUT_STATE},
     },
-    UI::Input::{KeyboardAndMouse::GetKeyState, XboxController::XINPUT_STATE},
+    core::{PCSTR, PCWSTR},
 };
 
 /// Returns 1 if pressed, 0 if not pressed
@@ -22,8 +30,6 @@ pub static HOOK: StaticDetour<unsafe extern "system" fn(u32, *mut XINPUT_STATE) 
     }
     StaticDetour::__new(ffi_detour)
 };
-
-windows::core::link!("xinput1_4.dll" "system" fn XInputGetState(dwuserindex: u32, pstate: *mut XINPUT_STATE) -> u32);
 
 pub fn xinput_get_state_hook(dw_user_index: u32, pstate: *mut XINPUT_STATE) -> u32 {
     let result = unsafe { HOOK.call(dw_user_index, pstate) };
@@ -44,13 +50,40 @@ pub extern "system" fn DllMain(_: HINSTANCE, fdw_reason: u32, _: *const c_void) 
     match fdw_reason {
         DLL_PROCESS_ATTACH => unsafe {
             AllocConsole().unwrap();
-            HOOK.initialize(XInputGetState, xinput_get_state_hook)
-                .unwrap()
-                .enable()
-                .unwrap();
+            HOOK.initialize(
+                mem::transmute(find_xinput_get_state_address().unwrap()),
+                xinput_get_state_hook,
+            )
+            .unwrap()
+            .enable()
+            .unwrap();
         },
         DLL_PROCESS_DETACH => unsafe { HOOK.disable().unwrap() },
         _ => {}
     }
     true
+}
+
+fn find_xinput_get_state_address() -> Option<usize> {
+    for lib_name in [
+        "xinput1_4.dll\0",
+        "xinput1_3.dll\0",
+        "xinput1_2.dll\0",
+        "xinput1_1.dll\0",
+        "xinput9_1_0.dll\0",
+    ] {
+        if let Some(address) = get_module_symbol_address(lib_name, c"XInputGetState") {
+            return Some(address);
+        }
+    }
+
+    None
+}
+
+fn get_module_symbol_address(module: &str, symbol: &CStr) -> Option<usize> {
+    let module: Vec<_> = OsStr::new(module).encode_wide().collect();
+    unsafe {
+        let handle = GetModuleHandleW(PCWSTR(module.as_ptr())).ok()?;
+        GetProcAddress(handle, PCSTR(symbol.as_ptr().cast())).map(|addr| addr as usize)
+    }
 }
